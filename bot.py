b5060743c790dc426171bbc20921956443bd506c
Hippes <71636909+Hippes@users.noreply.github.com>
2026-01-12 13:00:30 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 # Конфигурация
 BOT_TOKEN = "8372625344:AAGVbxGAbrz6bquGDwJ6pSJaYOPZFNfriPI"  # Замените на токен вашего бота
-WEBAPP_URL = "https://your-game.vercel.app"  # URL вашей игры после деплоя
+WEBAPP_URL = "https://ny-game-miniapp.vercel.app/"  # URL вашей игры после деплоя
 
 # Инициализация бота
 bot = Bot(token=BOT_TOKEN)
